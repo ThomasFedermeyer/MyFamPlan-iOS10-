7966ddb7d32d1bdac1689758b60cb38669e7bcf4
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2020-02-24 22:02:33 -0600

    Initial Commit

---
+++ FamPlan test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FamPlan test
+//
+//  Created by Eddie Federmeyer on 2/24/20.
+//  Copyright © 2020 Eddie Federmeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
